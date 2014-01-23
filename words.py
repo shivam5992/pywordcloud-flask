13efed6ee207205bf74ddf6b802fb97e0c3ce32c
Shivam Bansal <shivam5992@gmail.com>
2014-01-23 14:55:44 +0530

    flask app created

--- words.py
+++ words.py
@@ -1,12 +1,63 @@
+from flask import Flask, render_template, request, flash
 from BeautifulSoup import BeautifulSoup
 import urllib
 import random
 
-'''
-function to get top keywords from the whole text
-'''
+app = Flask(__name__)
+app.secret_key = 'You will never guess'
+
+@app.route('/', methods = ['GET','POST'])
+@app.route('/index', methods = ['GET','POST'])
+def index():
+	# create word cloud if proper url is received, error handling for url
+	if request.method == 'POST':
+		url = request.form['urllink']
+		htmltext = urllib.urlopen(url).read()
+		soup = BeautifulSoup(htmltext)
+
+		article = ""
+		for text in soup.findAll(text=True):
+			article += text.encode("utf-8")
+		a = getKeywords(article)
+		random.shuffle(a)
+
+		span = ""
+		css  = """#box{font-family:'calibri';max-height:1000px;max-width:1000px;border:2px solid black;}
+		#box a{text-decoration : none}
+		"""
+		
+		colors = ['#607ec5','#002a8b','#86a0dc','#4c6db9']
+		k = 0
+		for index,item in enumerate(a):
+			index += 1
+			span += '<a href=#><span class="word'+str(index)+'" id="tag'+str(index)+'">' + str(item[0]) + "</span></a>\n"
+			size = item[1]*item[1]*item[1]%400
+			if size < 50 and k%2 != 0:
+				size += 150
+		 	if size > 50 and size < 100 and k%2 != 0:
+				size += 100
+		 	css += '#tag'+str(index)+'{font-size: '+ str(size) +'%;color: '+colors[int(k%4)]+'}\n'
+		 	css += '#tag'+str(index)+':hover{color: red}\n'
+		 	k += 1
+
+		f = open('templates/wordcloud.html', 'w')
+		message = """
+		<link rel='stylesheet' href='static/wordcloud.css'>
+		<div id='box'>
+			""" + span +  """
+		</div>
+		"""
+		f.write(message)
+		f.close
+		
+		f = open('static/wordcloud.css', 'w')
+		f.write(css)
+		f.close
+		flash("wordcloud created")
+		return render_template('index.html')
+	return render_template('index.html')
 def getKeywords(articletext):
-	common = open("common_words.txt").read().split('\n')
+	common = open("static/common_words.txt").read().split('\n')
 	word_dict = {}
 	word_list = articletext.lower().split()
 	for word in word_list:
@@ -16,80 +67,12 @@ def getKeywords(articletext):
 			if word in word_dict:
 				word_dict[word] += 1
 	top_words =  sorted(word_dict.items(),key=lambda(k,v):(v,k),reverse=True)[0:50]
-	top20 = []
+	top = []
 	for w in top_words:
-		top20.append(w)
-	return top20
-
-url = "http://sethgodin.typepad.com/"
-htmltext = urllib.urlopen(url).read()
-soup = BeautifulSoup(htmltext)
-
-'''
-Collect all text from url
-'''
-article = ""
-for text in soup.findAll(text=True):
-	article += text.encode("utf-8")
-
-a = getKeywords(article)
-random.shuffle(a)
-
-span = ""
-css  = """#box{font-family:'calibri';max-height:1000px;max-width:1000px;border:2px solid black;}
-#box a{text-decoration : none}
-"""
-
-colors = ['#607ec5','#002a8b','#86a0dc','#4c6db9']
-k = 0
-for index,item in enumerate(a):
-	index += 1
-	span += '<a href=#><span class="word'+str(index)+'" id="tag'+str(index)+'">' + str(item[0]) + "</span></a>\n"
-	size = item[1]*item[1]*item[1]%600
-	if size < 100:
-		size += 50
- 	css += '#tag'+str(index)+'{font-size: '+ str(size) +'%;color: '+colors[int(k%4)]+'}\n'
- 	css += '#tag'+str(index)+':hover{color: red}\n'
- 	k += 1
-
-f = open('wordcloud.html', 'w')
-message = """<html>
-<head>
-<title>WordCloud | HTML</title>
-<link rel='stylesheet' href='style.css'>
-</head>
-<body>
-<div id='box'>
-
-""" + span +  """
-</div>
-</body>
-</html>"""
-f.write(message)
-f.close
-
-f = open('style.css', 'w')
-f.write(css)
-f.close
+		top.append(w)
+	return top
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+	app.run(debug = True)
